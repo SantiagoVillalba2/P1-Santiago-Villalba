e4ef572ad653290a519ff6e621f856abb0a48b94
SantiagoVillalba2 <ricardoleiva1943@gmail.com>
2026-04-10 15:32:18 -0300

    Add files via upload

---
+++ amazing_densor_juttuli1.ino
@@ -0,0 +1,24 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(12, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(12, HIGH);
+  delay(150);
+  digitalWrite(12, LOW);
+  delay(150);
+  digitalWrite(12, HIGH);
+  delay(150);
+  digitalWrite(12, LOW);
+  delay(150);
+  digitalWrite(12, HIGH);
+  delay(150);
+  digitalWrite(12, LOW);
+  delay(150);
+  digitalWrite(12, HIGH);
+  delay(5000);
+}
